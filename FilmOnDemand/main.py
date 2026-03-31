452fbe5abb5ce1035380f9fdbc07db5210b4b9c2
rdifeli <rdifeli@uwo.ca>
2026-03-31 05:30:17 -0400

    updated code to work with watchmode branch changes

--- FilmOnDemand/main.py
+++ FilmOnDemand/main.py
@@ -1,37 +1,44 @@
-from WatchmodeAPI.main import WatchmodeAPI
-
-from TraktAPI.main import TraktAPI
-from TMDbAPI.tmdb import TMDbAPI
-from TasteDiveAPI.main import TasteDiveAPI
+from WatchmodeAPI import WatchmodeAPI
 
 
 class FilmOnDemand:
     def __init__(self):
         self.watchmode = WatchmodeAPI()
-        self.tmdb = TMDbAPI()
-        self.trakt = TraktAPI()
-        self.tastedive = TasteDiveAPI()
 
     def settings(self):
         return None
 
     def get_watchmode_movies(self):
-        source_ids = self.watchmode.get_genre_or_source_ids(self.watchmode.source_data, ["netflix", "amazon"])
-        sort_setting = (input("Sort by Genres or Actor?\n")).lower().strip()
+        source_ids = self.watchmode.get_source_ids(["netflix", "amazon"])
+        sort_setting = input("Sort by Genres or Actor/Actress?\n").lower().strip()
+
         if sort_setting == "genre" or sort_setting == "genres":
-            genre_ids = self.watchmode.get_genre_or_source_ids(self.watchmode.genre_data, ["romance","comedy"])
+            genre_ids = self.watchmode.get_genre_ids(["romance", "comedy"])
             data = self.watchmode.fetch_movies_by_genre(genre_ids, source_ids)
-        elif sort_setting == "actor":
-            actor_name = input("Enter Actor Name: ") # right now assuming name exists
-            actor_id = self.watchmode.get_genre_or_source_ids(actor_name)
+
+        elif sort_setting == "actor" or sort_setting == "actress":
+            actor_name = input("Enter Actor Name: ").lower().strip()
+            actor_id = self.watchmode.get_actor_id(actor_name)
+            print("actor_id: ")
+            print(actor_id)
+            
+            if actor_id is None:
+                print("Actor not found.")
+                return []
+
             data = self.watchmode.fetch_movies_by_actor(actor_id, source_ids)
-        
-        movies = self.watchmode.parse_results(data) # take the data and parse the results to top 10
-        
+
+        else:
+            print("Invalid option.")
+            return []
+
+        movies = self.watchmode.parse_results(data)
+        print(movies)
         return movies
 
     def get_tastedive_movies(self):
-        return self.tastedive.get_similar_movies()
+        # something like return self.tastedive.get_similar_movies()
+        return None
 
     def get_movie_info(self):
         return None
@@ -41,7 +48,7 @@ class FilmOnDemand:
 
 if __name__ == "__main__":
     var = FilmOnDemand()
-    
+    var.get_watchmode_movies()
     # Take user input, decide on what settings they would like to set up:
     # --> if the user wants top 10 movies by genre, actor or movies similar to a movie provided
     # --> input what streaming services the user has access to
