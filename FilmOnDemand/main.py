2b98e1c6664ef9c8ceb4a9239bf1d7033c192db4
Samuel Huang <64177337+VinceJH@users.noreply.github.com>
2026-04-04 16:01:00 -0400

    Update the imports

--- FilmOnDemand/main.py
+++ FilmOnDemand/main.py
@@ -1,7 +1,13 @@
-from WatchmodeAPI import WatchmodeAPI
-from TraktAPI import TraktAPI
-from TMDbAPI import TMDbAPI
-from TasteDiveAPI import TasteDiveAPI
+import sys
+import os
+
+# Add the parent directory to Python's path so we can import the sibling folders
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from WatchmodeAPI.main import WatchmodeAPI
+from TraktAPI.main import TraktAPI
+from TMDbAPI.main import TMDbAPI
+from TasteDiveAPI.main import TasteDiveAPI
 
 
 class FilmOnDemand:
